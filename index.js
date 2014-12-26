04cf89a594b0c1d5a685af1d6dbf16812d40ad1d
Alex Yursha <alexyursha@gmail.com>
2014-12-26 14:22:41 +0300

    Make it work

---
+++ index.js
@@ -0,0 +1,46 @@
+/**
+ * Searches a file in a directory (recursively)
+ */
+
+var fs = require('fs')
+var log = require('y-log')
+
+var args = process.argv.slice(2); // strip node and program names
+var what = args[0];
+var where = args[1] || '.';
+
+fs.stat(where, function (err, stats) {
+	if (err) {
+		console.error(err);
+	} else {
+		if (!stats.isDirectory()) {
+			log.error(dir + ' is not a directory');
+		} else {
+			traverseDirectory(where);
+		}
+	}
+})
+
+function traverseDirectory (dir) {
+	fs.readdir(dir, function (err, files) {
+		if (err) {
+			console.error(err)
+		} else {
+			files.forEach(function (element, index, array) {
+				fs.stat(dir + '/' + element, function (err, stats) {
+					if (err) {
+						console.error(err)
+					} else {
+						if (stats.isDirectory()) {
+							traverseDirectory(dir + '/' + element) // recurse here
+						} else {
+                            if (element.indexOf(what) !== -1) {
+                                log.success(dir + '/' + element);
+                            }
+						}
+					}
+				})
+			})
+		}
+	})
+}
